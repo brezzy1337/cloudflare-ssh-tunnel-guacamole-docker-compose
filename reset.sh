1f13d72c8eaf8e894b0172358e804c9aa62a4dbb
Devin Phat <90067281+brezzy1337@users.noreply.github.com>
2024-04-19 17:14:36 -0500

    syntax

--- reset.sh
+++ reset.sh
@@ -2,7 +2,6 @@
 echo "This will delete your existing database (./data/)"
 echo "          delete your recordings        (./record/)"
 echo "          delete your drive files       (./drive/)"
-echo "          delete your certs files       (./nginx/ssl/)"
 echo ""
 read -p "Are you sure? " -n 1 -r
 echo ""   # (optional) move to a new line
