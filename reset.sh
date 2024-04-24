4be6092b221bf6d9ed03cba2ce12a477b8a9d05b
Devin Phat <90067281+brezzy1337@users.noreply.github.com>
2024-04-24 16:46:25 -0500

    Update reset.sh

--- reset.sh
+++ reset.sh
@@ -2,6 +2,7 @@
 echo "This will delete your existing database (./data/)"
 echo "          delete your recordings        (./record/)"
 echo "          delete your drive files       (./drive/)"
+echo "          delete your certs files       (./nginx/ssl/)"
 echo ""
 read -p "Are you sure? " -n 1 -r
 echo ""   # (optional) move to a new line
@@ -9,4 +10,3 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then # do dangerous stuff
  chmod -R +x -- ./init
  sudo rm -r -f ./data/ ./drive/ ./record/ ./nginx/ssl/
 fi
-
